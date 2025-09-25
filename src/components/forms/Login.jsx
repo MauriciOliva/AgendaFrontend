158ad07865ea5b66269a69018facf7c4691d8e9f
MauriciOliva <hectoroliva841@gmal.com>
2025-09-25 12:20:46 -0600

    Cambio de titulo

--- src/components/forms/Login.jsx
+++ src/components/forms/Login.jsx
@@ -31,7 +31,7 @@ export const Login = ({ onSubmit, error, loading }) => { // ← Recibir props
                 {/* Header */}
                 <div>
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                        Sistema de Viáticos
+                        Agenda Web
                     </h2>
                     <p className="mt-2 text-center text-sm text-gray-600">
                         Ingresa tus credenciales para acceder
